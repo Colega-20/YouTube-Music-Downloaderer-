85ad51c8413eea109a4a2d3f9182b1c41fb7f488
Colega-20 <157868652+Colega-20@users.noreply.github.com>
2024-11-13 02:05:22 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -14,44 +14,41 @@ var ctx = c.getContext("2d");
 
 var hueFw = false;
 var hue = -0.01;
+var font_size = 16;
+var drops = [];
 
-// making the canvas full screen
-c.height = window.innerHeight;
-c.width = window.innerWidth;
+function resizeCanvas() {
+  // Ajustar el tamaño del canvas para que sea un poco más alto que la ventana
+  c.width = window.innerWidth;
+  c.height = window.innerHeight + 16; // 100px más alto que la altura de la ventana
+
+  // Recalcular el número de columnas
+  var columns = c.width / font_size;
+  drops = [];
+  for (var x = 0; x < columns; x++) drops[x] = 1;
+}
+
+// Llamamos a la función de ajuste del canvas inicialmente y también al redimensionar la ventana
+resizeCanvas();
+window.addEventListener("resize", resizeCanvas);
 
 // the characters
 var konkani =
   "゠アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレワヰヱヲンヺ・ーヽヿ0123456789";
-// converting the string into an array of single characters
 var characters = konkani.split("");
-var font_size = 16;
-var columns = c.width / font_size; // number of columns for the rain
-var gradient = ctx.createLinearGradient(0, 10, 0, 200);
-// an array of drops - one per column
-var drops = [];
-// x below is the x coordinate
-// 1 = y-coordinate of the drop (same for every drop initially)
-for (var x = 0; x < columns; x++) drops[x] = 1;
 
 // drawing the characters
 function draw() {
-  // Get the BG color based on the current time i.e. rgb(hh, mm, ss)
-  // translucent BG to show trail
-
-  ctx.fillStyle = "rgba(0,0,0, 0.05)";
+  ctx.fillStyle = "rgba(0,0,0, 0.1)";
   ctx.fillRect(0, 0, c.width, c.height);
 
-  ctx.fillStyle = "#BBB"; // grey text
+  ctx.fillStyle = "#BBB";
   ctx.font = font_size + "px arial";
 
-  // looping over drops
   for (var i = 0; i < drops.length; i++) {
-    // background color
-    ctx.fillStyle = "rgba(10,10,10, 1)";
+    ctx.fillStyle = "rgba(10,10,10, 0)";
     ctx.fillRect(i * font_size, drops[i] * font_size, font_size, font_size);
-    // a random chinese character to print
     var text = characters[Math.floor(Math.random() * characters.length)];
-    // x = i * font_size, y = value of drops[i] * font_size
 
     if (root.rainbow) {
       hue += hueFw ? 0.01 : -0.01;
@@ -71,17 +68,10 @@ function draw() {
     }
 
     ctx.fillText(text, i * font_size, drops[i] * font_size);
-    // Incrementing Y coordinate
     drops[i]++;
-    // sending the drop back to the top randomly after it has crossed the screen
-    // adding randomness to the reset to make the drops scattered on the Y axis
     if (drops[i] * font_size > c.height && Math.random() > 0.975) drops[i] = 0;
   }
 }
-
-window.onresize = () => {
-  location.reload();
-};
 
 setInterval(draw, root.matrixspeed);
 
